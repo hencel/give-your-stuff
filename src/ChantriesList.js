058142b28525aba2b1e0f8bdf64f221467831325
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2019-07-23 23:10:14 +0200

    Page Structure ready

---
+++ src/ChantriesList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ChantriesList extends Component {
+  render() {
+    return (
+      <div className={'ChantriesList'}></div>
+    )
+  }
+}
+
+export default ChantriesList;
---
+++ src/FourStepsInfo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FourStepsInfo extends Component {
+  render() {
+    return (
+      <div className={'FourStepsInfo'}></div>
+    )
+  }
+}
+
+export default FourStepsInfo;
