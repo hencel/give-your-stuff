98bb054005445a5748745490e50157db1c8cb73b
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2019-07-25 23:52:48 +0200

    Header with everything elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.scss';
 import Header from './Header';
 import StatsInfo from './StatsInfo';
 import FourStepsInfo from './FourStepsInfo';
