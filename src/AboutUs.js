01c57b7f410779356d451904110e2e775dc5530b
Zbigniew Piernik <zbigniewpiernik@gmail.com>
2019-07-30 10:31:01 +0200

    About Us pannel added

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
+import decoration from './images/decoration.png';
+import about from './images/about.png';
+import signature from './images/signature.png';
+
 
 class AboutUs extends Component {
   render() {
     return (
-      <div className={'AboutUs'}></div>
+      <div className={'AboutUs'}>
+        <div>
+          <h2>O Nas</h2>
+          <img src={decoration} alt={'Decoration element'} />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu justo nulla. Nulla facilisi.
+            <img src={signature} alt={'Member sign'}/>
+          </p>
+        </div>
+        <div>
+          <img src={about} alt={'All people together'}/>
+        </div>
+      </div>
     )
   }
 }
